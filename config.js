4a2bd03f97f7a197bcfd83c8a8c9526902930da2
daisycho623 <daisycho623@gmail.com>
2026-09-09 22:54:01 +0900

    260909 - api 연동

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.WORK_LEDGER_CONFIG = { apiUrl: "" };
+window.WORK_LEDGER_CONFIG = { apiUrl: "https://script.google.com/macros/s/AKfycbz6R3ygxEjs2WfJNfRh52MhFl-t6yham_v7c8U8ipigkutgMVxH2-9qYyeWD9a6oEoB6g/exec" };
